31b452bc036398676c2a064a28984e6c623d6c91
kim <se6816@naver.com>
2022-11-04 02:33:29 +0900

    user 수정

--- springSecurity/src/main/java/com/example/security/domain/User.java
+++ springSecurity/src/main/java/com/example/security/domain/User.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -20,6 +21,7 @@ import lombok.Setter;
 @Data
 @NoArgsConstructor
 @Entity
+@Table(name="user")
 public class User {
 	@Id // primary key
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
