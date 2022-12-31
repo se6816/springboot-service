e69a933749f138edd93c4ecc1dd95ddb8396e88f
se6816 <60026427+se6816@users.noreply.github.com>
2023-01-01 04:42:30 +0900

    Update health.sh
    health.sh 오타 수정

--- springSecurity/scripts/health.sh
+++ springSecurity/scripts/health.sh
@@ -3,7 +3,7 @@
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)           # 현재 stop.sh가 속해있는 경로를 찾습니다.
 source ${ABSDIR}/profile.sh          # 자바로 보면 import와 같습니다. profile.sh에 있는 여러 function을 사용할 수 있도록 가져옵니다.
-source $(ABSDIR}/switch.sh
+source ${ABSDIR}/switch.sh
 
 IDLE_PORT=$(find_idle_port)
 
