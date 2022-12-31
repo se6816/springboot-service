9ef6e64bc089ea477201db21460b5c8f60f1c669
se6816 <60026427+se6816@users.noreply.github.com>
2023-01-01 04:27:51 +0900

    Update stop.sh
    stop.sh 수정

--- springSecurity/scripts/stop.sh
+++ springSecurity/scripts/stop.sh
@@ -6,14 +6,14 @@ source ${ABSDIR}/profile.sh          # 자바로 보면 import와 같습니다. 
 
 IDLE_PORT=$(find_idle_port)
 
-echo "> $IDLE_PORT에서 구동 중인 애플리케이션 확인"
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
-if[ -z ${IDLE_PID}]
+if [ -z ${IDLE_PID} ]
 then
-  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> KILL -15 $IDLE_PID"
+  echo "> kill -15 $IDLE_PID"
   kill -15 ${IDLE_PID}
   sleep 5
 fi
